4498f3a0b947a7e271ec02613cf5cb328e5c82d6
Maximilian Gaedig <mg@maximiliangaedig.com>
2022-08-03 00:38:05 +0200

    💬 Add notice about skill assesment

--- pages/index.tsx
+++ pages/index.tsx
@@ -297,6 +297,18 @@ const Home: NextPage<Props> = ({ views }: Props) => {
           Views:
           {' '}
           {views}
+          <br />
+          <br />
+          <span className="text-base">
+            My usage frequency and experience of my skills is mostly based on self assessment,
+            experience is on a scale from 0-9,
+            {' '}
+            of which 9 is knowing everything and 0 is knowing nothing.
+            <br />
+            In the future I plan to replace this with
+            {' '}
+            a widely used assessment system if I find one.
+          </span>
         </p>
       </footer>
     </div>
